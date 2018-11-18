3aac910ceefd6581ac8faebfd79aa774a26d27e7
husen233 <sen.hu@biz-united.com.cn>
2018-11-18 13:00:24 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ssaw-turbine-stream
---
+++ src/main/java/com/ssaw/ssawturbinestream/TurbineStreamApplication.java
@@ -0,0 +1,17 @@
+package com.ssaw.ssawturbinestream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+
+/**
+ * @author HS
+ */
+@SpringBootApplication
+@EnableTurbineStream
+public class TurbineStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineStreamApplication.class, args);
+    }
+}
